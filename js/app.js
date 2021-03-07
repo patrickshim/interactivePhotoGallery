cdbf73432f536ffbcaa42a4940e2dfbe2744e163
shimp817 <shimp@oregonstate.edu>
2021-03-07 18:20:55 -0500

    requirements fulfilled until meets expections

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-baguetteBox.run('.gallery');
 const search = new Filter('search', 'data-caption');
 
+baguetteBox.run('.gallery', {
+  buttons: 'auto',
+  captions: true,
+  animation: 'fadeIn'
+});
